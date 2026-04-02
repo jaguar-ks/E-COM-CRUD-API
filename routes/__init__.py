35b78188794f20e9940c96f7864fc61cd9c0eed0
jaguar-ks <fahdamineksouss@gmail.com>
2026-04-02 11:10:51 +0100

    feat(routes): export router modules

---
+++ routes/__init__.py
@@ -0,0 +1,5 @@
+from .products import router as products_router
+from .categories import router as categories_router
+from .customers import router as customers_router
+from .orders import router as orders_router
+from .order_items import router as order_items_router
